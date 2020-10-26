28521674bae298cf7f643d03efe4477c0fd711c0
SinaisAna <anaguanda20@gmail.com>
2020-10-25 23:50:08 -0300

    errores en los test

--- test/comenst.spec.js
+++ test/comenst.spec.js
@@ -1,45 +1,54 @@
 import MockFirebase from 'mock-cloud-firestore';
-import { readAddNotesToDB, editTextPost, deletePost } from '../src/lib/firebase/firestore.js';
+
+import {
+  readAddNotesToDB, addNotesToDB,
+} from '../src/lib/firebase/firestore.js';
 
 const fixtureData = {
   __collection__: {
-    publications: {
+    posts: {
       __doc__: {
-        abc1d: {
-          note: 'Hello World',
-          date: '',
+        a01: {
+          uid: 'user1',
+          user: 'Mariana',
+          url: '',
+          privacy: '0',
+          photo: '',
+          likes: [],
+          email: '',
+          datetime: '202051919282',
+          date: '19/5/2020',
+          content: 'Probando post',
         },
-
-        abc2d: {
-          note: 'Good morning',
-          date: '',
+        a02: {
+          uid: 'user2',
+          user: 'Edward',
+          url: '',
+          privacy: '1',
+          photo: '',
+          likes: [],
+          email: '',
+          datetime: '202051919282',
+          date: '19/5/2020',
+          content: 'Probando post',
         },
-
       },
     },
   },
 };
-
 global.firebase = new MockFirebase(fixtureData, { isNaiveSnapshotListenerEnabled: true });
 
-describe('Edit a post', () => {
-  it('Should be able to edit a post', (done) => editTextPost('abc1d', 'Hello wonderful world', '')
-    .then(() => readAddNotesToDB(
-      (data) => {
-        const result = data.find((publications) => publications.note === 'Hello wonderful world');
-        expect(result.note).toBe('Hello wonderful world');
+describe('Publicar post', () => {
+  // eslint-disable-next-line no-unused-vars
+  it('Debería poder publicar un post', (done) => addNotesToDB('user3', 'Mirella', '', '0', '', 'prueba hola', '', '', '', [])
+    .then(() => {
+      const callback = (posts) => {
+        const result = posts.find((element) => element.content === 'prueba hola');
+        // eslint-disable-next-line no-console
+        console.log(result);
+        expect(result.user).toBe('user3');
         done();
-      },
-    )));
-});
-
-describe('Delete a post', () => {
-  it('Should be able to delete a post', (done) => deletePost('abc1d')
-    .then(() => readAddNotesToDB(
-      (data) => {
-        const result = data.find((publications) => publications.id === 'abc1d');
-        expect(result).toBe(undefined);
-        done();
-      },
-    )));
+      };
+      readAddNotesToDB(callback);
+    }));
 });
